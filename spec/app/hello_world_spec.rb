c8556cee338498fad0e059d85bd28cc6ae01fcf6
Alice Post <alicep2322@gmail.com>
2020-01-02 10:51:06 -0700

    Remove pry
    Co-Authored-By: Brian Bower <BabsLabs@users.noreply.github.com>
    Co-Authored-By: Michael Cooper <macoop2363@gmail.com>

--- spec/app/hello_world_spec.rb
+++ spec/app/hello_world_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe 'hello world' do
   it 'shows hello world' do
     get '/'
+
     expect(last_response).to be_ok
     expect(last_response.body).to eq("Hello World!")
   end
