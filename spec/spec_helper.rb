11ade5fcc3b8d36fefd1069ee3922ca76a6341bd
Michael Cooper <macoop2363@gmail.com>
2020-01-02 11:34:45 -0700

    Configure vcr to record new episodes for cassettes
    Co-Authored-By: Brian Bower <BabsLabs@users.noreply.github.com>
    Co-Authored-By: Alice Post <ap2322@users.noreply.github.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,7 @@ VCR.configure do |config|
   config.cassette_library_dir = 'spec/cassettes'
   config.hook_into :webmock
   config.configure_rspec_metadata!
+  config.default_cassette_options = { record: :new_episodes }
   config.filter_sensitive_data("<GITHUB_TEST_TOKEN>") { ENV['GITHUB_TEST_TOKEN'] }
   config.filter_sensitive_data("<MUSIX_MATCH_TOKEN>") { ENV['MUSIX_MATCH_TOKEN'] }
   config.filter_sensitive_data("<WATSON_TOKEN>") { ENV['WATSON_TOKEN'] }
