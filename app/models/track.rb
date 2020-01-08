303cd0752cff2fd3a3059a8f11e84daffcee1e42
Brian Bower <brian@brianbower.com>
2020-01-07 19:49:46 -0700

    Add relationships to model

--- app/models/track.rb
+++ app/models/track.rb
@@ -5,4 +5,7 @@ class Track < ActiveRecord::Base
   validates_presence_of :mm_artist_id
   validates_presence_of :artist_name
 
+  has_many :track_sentiments
+  has_many :sentiments, through: :track_sentiments
+
 end
