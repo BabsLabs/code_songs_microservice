2b6beb566bc5b1bd203160745b0e3f55f9396d81
Alice Post <alicep2322@gmail.com>
2020-01-08 11:24:37 -0700

    A track can fetch its lyrics and make its own sentiments with lyrics

--- app/models/track.rb
+++ app/models/track.rb
@@ -7,4 +7,41 @@ class Track < ActiveRecord::Base
 
   has_many :sentiments
 
+  def lyrics
+    service = MusixMatchService.new(nil, mm_track_id)
+    response = service.get_lyrics
+    lyrics = response[:message][:body][:lyrics][:lyrics_body]
+    lyric_sanitizer(lyrics)
+  end
+
+  def make_sentiments(lyrics_text)
+    service = WatsonService.new(lyrics_text)
+    sentiment_response = service.sentiment[:sentences_tone]
+
+    tones_hash(sentiment_response).each do |k,v|
+      sentiments.create(name: k.to_s, value: v)
+    end
+
+  end
+
+  private
+
+  def tones_hash(sentiments_data)
+    tones_hash = {}
+    sentiments_data.each do |sentences|
+      sentences[:tones].each do |tone|
+        if tones_hash[tone[:tone_id].to_sym]
+          tones_hash[tone[:tone_id].to_sym] += tone[:score]
+        else
+          tones_hash[tone[:tone_id].to_sym] = tone[:score]
+        end
+      end
+    end
+
+    tones_hash
+  end
+
+  def lyric_sanitizer(song_lyrics)
+    song_lyrics.gsub("\n\n******* This Lyrics is NOT for Commercial use *******\n", "")
+  end
 end
