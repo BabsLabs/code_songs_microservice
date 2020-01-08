6071e04d152c1c7ab01a53c2256e30913ab531f7
Michael Cooper <macoop2363@gmail.com>
2020-01-08 16:38:29 -0700

    Change test to update with top 100 songs and for the endpoint to only add tracks that don't exist in the database

--- spec/app/update_database_spec.rb
+++ spec/app/update_database_spec.rb
@@ -1,7 +1,17 @@
 require 'spec_helper'
 
 describe 'When a post request is triggered to /update_database' do
-  it 'should add a track to the database' do
-    expect{post '/update_database'}.to change{Track.count}.by(1)
+  it 'should add a track to the database', :vcr do
+    expect{post '/update_database'}.to change{Track.count}.by(100)
+  end
+
+  it 'should only add tracks if they do not exist already', :vcr do
+    Track.create(
+      title: 'Dance Monkey',
+      mm_track_id: 177501051,
+      mm_artist_id: 37843472,
+      artist_name: 'Tones and I'
+    )
+    expect{post '/update_database'}.to change{Track.count}.by(99)
   end
 end
