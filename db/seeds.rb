fcd06ac93a3c641242da41a570d6ec5af3544b6c
Alice Post <alicep2322@gmail.com>
2020-01-08 09:33:19 -0700

    Add seeds and info to sentiment poro

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
 require './app/models/track.rb'
 
 t1 = Track.create(title: "Rocket Man!", mm_track_id: '10', mm_artist_id: '5', artist_name: 'Elton John')
+
+t2 = Track.create(title: 'Umbrella', mm_track_id: '32130011', mm_artist_id: '33491890', artist_name: 'Rhianna')
+s2 = t2.sentiments.create(:joy=>2.13323, :tentative=>1.840974, :analytical=>5.124888, :confident=>4.398569, :fear=>0.688855, :sadness=>0.620114)
