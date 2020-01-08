f5f6348d88112082f7c98ea868638573636a34a8
Alice Post <alicep2322@gmail.com>
2020-01-08 15:57:06 -0700

    Add '/codesongs_matcher' endpoint with extrapolated headers and pseudocode

--- app.rb
+++ app.rb
@@ -21,3 +21,32 @@ post '/update_database' do
     artist_name: 'Michael Cooper'
   )
 end
+
+get '/codesongs_matcher' do
+  # Get the info from the headers (login, artist_id, repo, token)
+  login = request.env['login']
+  artist_id = request.env['artist_id']
+  repo = request.env['repo']
+  token = request.env['token']
+
+  # repo = RepoFacade gets the commit messages
+  repo = ReposFacade.new(login, token, repo)
+  binding.pry
+
+  # repo_sentiments = repo.get_sentiments
+  # Send combined string to Watson for sentiments
+  # Get the info from the headers (login, artist_id, repo, token)
+  # Get repo commit messages
+  # get sentiments from the repo messages
+  # Get tracks_data from artist_id
+  # tracks_data.each |track_data|
+  #   if track not in db
+  #     new_track = Track.create(track_data)
+  #     new_track.get_youtube_link
+  #     sentiments_data = WatsonService.get_sentiments(track.lyrics)
+  #     sentiments_data.each |sentiment_data|
+  #       new_track.sentiments.create(sentiment_data)
+  #   end
+  # Do math with repo sentiments and all artist track sentiments?
+  # expected_output [ {title: 'title', link: 'link' } ]
+end
