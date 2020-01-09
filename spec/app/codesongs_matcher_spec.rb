ba269128d9d7ddef9baace277f6e7e7b909669d2
Alice Post <alicep2322@gmail.com>
2020-01-08 18:53:04 -0700

    Start test for the codesongs_matcher endpoint - this is the whole project

--- spec/app/codesongs_matcher_spec.rb
+++ spec/app/codesongs_matcher_spec.rb
@@ -1,7 +1,10 @@
 require 'spec_helper'
 
-describe "when the endpoint '/codesongs_matcher' is hit", :vcr do
-  it 'returns a JSON object formated as an ordered array of hashes with track name and link' do
+describe "when the endpoint '/codesongs_matcher' is hit" do
+  it 'returns a JSON object formated as an ordered array of hashes with track name and link', :vcr do
+    WebMock.enable_net_connect!
+    VCR.eject_cassette
+    VCR.turn_off!(ignore_cassettes: true)
     params = {'holder'=> 'holder'}
     headers = { 'login' => 'ap2322', 'token'=> ENV['GITHUB_TEST_TOKEN'], 'repo' => 'battleship', 'artist_id'=> '38620990'}
     get '/codesongs_matcher', params, headers
