780d16ad4e6555f8e6a21fd78fbad98b98e3909d
Alice Post <alicep2322@gmail.com>
2020-01-08 22:36:42 -0700

    Make test and cassette for the '/codesongs_matcher' endpoint - unable to replicate in Postman

--- spec/app/codesongs_matcher_spec.rb
+++ spec/app/codesongs_matcher_spec.rb
@@ -2,14 +2,13 @@ require 'spec_helper'
 
 describe "when the endpoint '/codesongs_matcher' is hit" do
   it 'returns a JSON object formated as an ordered array of hashes with track name and link', :vcr do
-    WebMock.enable_net_connect!
-    VCR.eject_cassette
-    VCR.turn_off!(ignore_cassettes: true)
-    params = {'holder'=> 'holder'}
+    params = nil
     headers = { 'login' => 'ap2322', 'token'=> ENV['GITHUB_TEST_TOKEN'], 'repo' => 'battleship', 'artist_id'=> '38620990'}
     get '/codesongs_matcher', params, headers
 
     expect(last_response).to be_ok
-    expect(last_response.body).to be_a JSON
+    # expect(last_response.body).to be_a JSON
+    expected_output = "[{\"title\":\"Shape of You\",\"link\":\"https://www.youtube.com/watch?v=kx7P_ENnDPE\"},{\"title\":\"Thinking Out Loud\",\"link\":\"https://www.youtube.com/watch?v=kx7P_ENnDPE\"},{\"title\":\"Photograph\",\"link\":\"https://www.youtube.com/watch?v=kx7P_ENnDPE\"},{\"title\":\"Perfect\",\"link\":\"https://www.youtube.com/watch?v=CrKRDEZExyU\"},{\"title\":\"South of the Border\",\"link\":\"https://www.youtube.com/watch?v=kx7P_ENnDPE\"}]"
+    expect(last_response.body).to eq expected_output
   end
 end
