8c8513e208912fdfea12ee010a71080f152b1c07
Michael Cooper <macoop2363@gmail.com>
2020-01-09 11:31:27 -0700

    Refactor updating the database with its own service object

--- app/services/database_updater_service.rb
+++ app/services/database_updater_service.rb
@@ -1,18 +1,35 @@
 class DatabaseUpdaterService
 
-  def self.update_database
-    tracks_data = MusixMatchService.get_top_songs[:message][:body][:track_list]
-
+  def self.update_database(page_size = 100, page_num = 1)
+    tracks_data = get_track_data(page_size, page_num)
+    
     tracks_data.each do |track_data|
-      track = track_data[:track]
-      if Track.find_by(mm_track_id: track[:track_id]).nil?
-        Track.create(
-          title: track[:track_name],
-          mm_track_id: track[:track_id],
-          mm_artist_id: track[:artist_id],
-          artist_name: track[:artist_name]
-        )
-      end
+      track_data = track_data[:track]
+      create_track(track_data) unless track_exists?(track_data[:track_id])
     end
+  end
+
+  private_class_method
+
+  def self.get_track_data(page_size, page_num)
+    tracks_data = MusixMatchService.get_top_songs(page_size, page_num)
+    tracks_data[:message][:body][:track_list]
+  end
+
+  def self.track_exists?(track_id)
+    Track.find_by(mm_track_id: track_id) != nil
+  end
+
+  def self.create_track(track_data)
+    Track.create(track_params(track_data))
+  end
+
+  def self.track_params(track_data)
+    {
+      title: track_data[:track_name],
+      mm_track_id: track_data[:track_id],
+      mm_artist_id: track_data[:artist_id],
+      artist_name: track_data[:artist_name]
+    }
   end
 end
