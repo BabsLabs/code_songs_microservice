cb67c4c76f31b3120cb4c0269658c800694bc036
Michael Cooper <macoop2363@gmail.com>
2020-01-09 15:38:09 -0700

    Add ability to update the database with more than just the top 100 songs

--- app/services/database_updater_service.rb
+++ app/services/database_updater_service.rb
@@ -1,4 +1,5 @@
 class DatabaseUpdaterService
+  @@page_num = 2
 
   def self.update_database(page_size = 100, page_num = 1)
     tracks_data = get_track_data(page_size, page_num)
@@ -7,6 +8,15 @@ class DatabaseUpdaterService
       track_data = track_data[:track]
       create_track(track_data) unless track_exists?(track_data[:track_id])
     end
+  end
+
+  def self.seed_database(page_size = 100)
+    update_database(page_size, current_page)
+    @@page_num += 1
+  end
+
+  def self.current_page
+    @@page_num
   end
 
   private_class_method
