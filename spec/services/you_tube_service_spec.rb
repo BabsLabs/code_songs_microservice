2dd5cfa67133d500dff200a27223329d89fe27f0
Brian Bower <brian@brianbower.com>
2020-01-09 23:21:47 -0700

    Fix youtube service to give more specific results.

--- spec/services/you_tube_service_spec.rb
+++ spec/services/you_tube_service_spec.rb
@@ -2,14 +2,14 @@ require 'spec_helper'
 
 describe YouTubeService do
   it 'fetches a video link', :vcr do
-    song_title = 'Rocket Man'
-    artist_name = 'Elton John'
+    song_title = 'Who Will Save Your Soul'
+    artist_name = 'Jewel'
     youtube_service = YouTubeService.new(song_title, artist_name)
 
     expect(youtube_service).to be_a YouTubeService
 
     expect(youtube_service.get_youtube_link).to be_a String
-    expect(youtube_service.get_youtube_link).to eq("https://www.youtube.com/embed/DtVBCG6ThDk")
+    expect(youtube_service.get_youtube_link).to eq("https://www.youtube.com/embed/0wBDDAZkNtk")
   end
 
   it 'sanitizes the name of an artist that uses non ASCII characters', :vcr do
