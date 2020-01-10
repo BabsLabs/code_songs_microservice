c410f550d5b249aa6393d8aae693363d4fbf8257
Alice Post <alicep2322@gmail.com>
2020-01-09 22:28:06 -0700

    Slightly different output with new cassette
    Co-Authored-By: Brian Bower <BabsLabs@users.noreply.github.com>
    Co-Authored-By: Michael Cooper <macoop2363@gmail.com>

--- spec/app/codesongs_matcher_spec.rb
+++ spec/app/codesongs_matcher_spec.rb
@@ -8,7 +8,7 @@ describe "when the endpoint '/codesongs_matcher' is hit" do
 
     expect(last_response).to be_ok
     # expect(last_response.body).to be_a JSON
-    expected_output = "{\"songs\":[{\"title\":\"Shape of You\",\"link\":\"https://www.youtube.com/embed/JGwWNGJdvx8\"},{\"title\":\"Thinking Out Loud\",\"link\":\"https://www.youtube.com/embed/lp-EO5I60KA\"},{\"title\":\"Photograph\",\"link\":\"https://www.youtube.com/embed/qgmXPCX4VzU\"},{\"title\":\"Perfect\",\"link\":\"https://www.youtube.com/embed/UDDMYw_IZnE\"},{\"title\":\"South of the Border\",\"link\":\"https://www.youtube.com/embed/UPOT2tgY9QQ\"}]}"
+    expected_output = "{\"songs\":[{\"title\":\"Shape of You\",\"link\":\"https://www.youtube.com/embed/JGwWNGJdvx8\"},{\"title\":\"Thinking Out Loud\",\"link\":\"https://www.youtube.com/embed/JGwWNGJdvx8\"},{\"title\":\"Photograph\",\"link\":\"https://www.youtube.com/embed/JGwWNGJdvx8\"},{\"title\":\"Perfect\",\"link\":\"https://www.youtube.com/embed/JGwWNGJdvx8\"},{\"title\":\"South of the Border\",\"link\":\"https://www.youtube.com/embed/JGwWNGJdvx8\"}]}"
     expect(last_response.body).to eq expected_output
   end
 
